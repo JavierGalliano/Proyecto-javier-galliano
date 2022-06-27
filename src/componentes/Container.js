e3397f76a3681ef00ed8cb0ff4dabf6509f50c89
JavierGalliano <javiergalliano1@gmail.com>
2022-06-27 19:03:27 -0300

    Se agrego el contador

--- src/componentes/Container.js
+++ src/componentes/Container.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Contador from "./Contador"
 function Container(props){
+    
 const {name} = props;
 return(
 <div>
     <h1>{name}</h1>
+    <Contador stock={5} initial ={0}/>
 </div>
 );
 }
